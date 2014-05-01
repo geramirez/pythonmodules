133b4563cd512e6555fce9eace9e38ab57bb16e3
ramirezg <gabriel.e.ramirez@gmail.com>
2014-05-01 12:50:01 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 #helpers
 
 import regex
+from pandas import ExcelWriter
 
 def count_uniques(df):
     return df.drop_duplicates().count()
@@ -11,9 +12,16 @@ def potentials(network):
 
 def replacer(user,key):
     if user in key.keys():
-        return key[user]
+        return regex.sub("\n|\r","",key[user])
     else: 
         return user
+
+def text_replacer(text,users):
+
+    for user in users.keys():
+        text = regex.sub(user,users[user],text)
+
+    return text
     
 def is_in_tweet_bank(tweet,tweetbank):
     for searchstring in tweetbank:
@@ -32,3 +40,46 @@ def is_in_user_bank_sigdos(user,userbank):
         if regex.sub("\W","",user) == regex.sub("\W","",searchstring):
             return user
     return "@" + user
+
+def pandas_to_edgelist(row):
+    #this function generates an edge list for networkx
+    return (row.connection,
+            row.from_user,
+            {"tweets":[{"id":row.id,
+                      "text":row.text,
+                      "hashtags":row.hashtags,
+                      "favorite_count":row.favorite_count,
+                      "in_reply_to_screen_name":row.in_reply_to_screen_name,
+                      "retweet_count":row.retweet_count,
+                      "media_type":row.media_type,
+                      "media_url":row.media_url,
+                      "user_mentions":row.user_mentions,
+                      "lang":row.lang,
+                      "in_reply_to_status_id_str":row.in_reply_to_status_id_str,
+                      "url":row.url,
+                      "created_at":row.created_at
+                    }]}
+            )
+
+def pandas_to_edgelist_uni(row):
+    #this function generates an edge list for networkx
+    return (row.connection,
+            row.from_user)
+
+def pandas_node_attributes(row):
+    #this function generates an edge list for networkx
+    return (row.from_user, 
+           {'user_lang' : row.user_lang,
+            'user_utc_offset': row.user_utc_offset,
+            'followers' : row.followers,
+            #'user_created_time' : row.user_created_time,
+            'user_statuses' : row.user_statuses,
+            'time_zone' : row.time_zone
+            }
+            )
+
+def save_xls(list_dfs, xls_path):
+    writer = ExcelWriter(xls_path)
+    for n, df in enumerate(list_dfs):
+        df.to_excel(writer,'sheet%s' % n)
+    writer.save()
